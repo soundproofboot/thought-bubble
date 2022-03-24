91cc3dbe95bf1fe7500b8f51606160a45caffd37
C Bares <colinedwinbares@gmail.com>
2022-03-24 13:51:42 -0500

    reaction model, controller method and route to add reaction to thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -36,6 +36,21 @@ const thoughtController = {
       })
   },
 
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought with this id' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  }
 }
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { getAllThoughts, createThought } = require('../../controllers/thought-controller');
+const { getAllThoughts, createThought, addReaction } = require('../../controllers/thought-controller');
 
 router
   .route('/:userId')
   .post(createThought);
 
+router
+  .route('/:userId/:thoughtId').put(addReaction)
 module.exports = router;
